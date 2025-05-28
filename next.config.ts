387176fb5fd96118038f559b99463093b3fb35be
galizaragoza <galizaragozadev@gmail.com>
2025-05-28 11:10:30 +0200

    Arreglo en tsconfig y nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import {NextConfig} from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {};
- 
-const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+// next.config.ts
+import { NextConfig } from "next";
+import path from "path";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const nextConfig: NextConfig = {
+  // Añadimos aquí la sección de webpack
+  webpack(config) {
+    config.resolve.alias = {
+      // preserva cualquier alias existente...
+      ...(config.resolve.alias ?? {}),
+      // y añade el tuyo:
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
+};
+
+export default createNextIntlPlugin()(nextConfig);
