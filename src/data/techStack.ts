8ab20078734843ba5fca335ab8bb7c269e50fa7a
galizaragoza <galizaragozadev@gmail.com>
2025-06-09 16:58:06 +0200

    Update tech stack

--- src/data/techStack.ts
+++ src/data/techStack.ts
@@ -1,10 +1,9 @@
 import { IconType } from "react-icons";
-import {
-  FaReact, FaHtml5, FaNodeJs
-} from "react-icons/fa";
+import { FaReact, FaHtml5, FaNodeJs, FaDocker } from "react-icons/fa";
 import { SiMongodb, SiNextdotjs, SiTypescript } from "react-icons/si";
 import { IoLogoJavascript } from "react-icons/io5";
 import { AiFillOpenAI } from "react-icons/ai";
+import { BiLogoPostgresql } from "react-icons/bi";
 import { RiTailwindCssFill } from "react-icons/ri";
 
 export interface TechItem {
@@ -23,4 +22,6 @@ export const techStack: TechItem[] = [
   { Icon: SiNextdotjs, color: "#000000", label: "Next.js" },
   { Icon: IoLogoJavascript, color: "#f7e018", label: "JavaScript" },
   { Icon: AiFillOpenAI, color: "#000000", label: "OpenAI" },
+  { Icon: BiLogoPostgresql, color: "#336791", label: "PostgreSQL" },
+  { Icon: FaDocker, color: "#2496ed", label: "Docker" },
 ];
